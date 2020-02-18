b01eed2288e752fbc8fcb2a1d72747275e18e171
Vladyslav Kyrianov <Vladyslav_Kyrianov@epam.com>
2020-02-18 16:58:02 +0200

    132

--- automaionTask1/automaionTask1/Pages/Google.cs
+++ automaionTask1/automaionTask1/Pages/Google.cs
@@ -8,16 +8,35 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.Extensions;
+using OpenQA.Selenium.Support.PageObjects;
 using OpenQA.Selenium.Support.UI;
 using WDSE;
 using WDSE.Decorators;
 using WDSE.ScreenshotMaker;
+using System.IO;
 
 
 namespace automaionTask1
 {
-    class Google: SearchEngines
+    class Google : SearchEngines
     {
         public Google(IWebDriver driver) : base(driver) { }
+
+        protected override string folderWithScreenshots
+        { get => $""; }
+        protected string stringToSearch { get; set; }
+
+        protected override string currentUrl { get => "https://www.google.com/"; }
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='searchform']//input[@type='text']")]
+        private IWebElement SearchField;
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='pnnext']")]
+        protected new IWebElement NextPageButton;
+
+        [FindsBy(How=How.XPath,Using = "//*[contains(text(),'MTB БАНК')]")]
+        private IWebElement DesiredElement;
+
+        
     }
 }
