f10924104799475140d551cd03bb9660243f8855
Vladyslav Kyrianov <Vladyslav_Kyrianov@epam.com>
2020-05-01 23:47:09 +0300

    task update

--- PostmanAPI/PostmanAPI/Data.cs
+++ PostmanAPI/PostmanAPI/Data.cs
@@ -4,6 +4,6 @@
     {
        public const string BaseUrl = "https://api.getpostman.com";
        public const string CollectionSchema = "https://schema.getpostman.com/json/collection/v2.1.0/";
-        public const string CurrentxApiKey = "";
+        public const string CurrentxApiKey = "PMAK-5e988ea695df91003e6db77a-2ea7f6d668c2576e6c8fa162f0fa801332";
     }
 }
